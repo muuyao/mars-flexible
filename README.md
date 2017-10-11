664cd04c452dfeeba9e9a1bb354d92bc33d7005d
wangxiao <wanngxiao@gmail.com>
2017-10-11 17:40:49 +0800

    feat: 0.1.0 version

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# mars-flexible
+# Mars-flexible [![NPM version][npm-image]][npm-url]
+
+移动端自适应库，执行这个 js 后，会在 html（也就是document.documentElement）上增加一个 data-dpr 属性，以及 font-size 样式。之后页面中的元素，都可以用 rem 单位来设置。html 上的 font-size 就是 rem 的基准像素。
+
+该库主要做了两件事
+
+- 给<html>元素添加 `font-size` 属性，并且动态改写 `font-size` 的值
+- 给<html>元素添加 `data-dpr` 属性，并且动态改写 `data-dpr` 的值
+- 对支持0.5px border 的添加 `hairlines` 类名
+
+建议将该库直接做内联处理
+
+## Install
+
+```bash
+$ npm install mars-flexible --save
+```
+
+## Usage
+```
+import 'mars-flexible';
+```
+
+## 参考
+- [淘宝移动端适配方案](https://github.com/amfe/article/issues/17)
+
+[npm-image]: https://badge.fury.io/js/generator-vapp.svg
+[npm-url]: https://npmjs.org/package/mars-flexible
+
---
+++ index.js
@@ -0,0 +1,67 @@
+/**
+ * 移动端适配方案
+ * 1rem = viewWidth / 10
+ *
+ * 参考：手淘(https://github.com/amfe/article/issues/17) 移动端适配方案
+ */
+(function (win, doc) {
+  var docEl = doc.documentElement;
+  var dpr = parseInt(win.devicePixelRatio || 1, 10);
+  var tid;
+
+  // adjust body font size
+  function setBodyFontSize() {
+    if (document.body) {
+      document.body.style.fontSize = (12 * dpr) + 'px'
+    }
+    else {
+      document.addEventListener('DOMContentLoaded', setBodyFontSize)
+    }
+  }
+  setBodyFontSize();
+
+  docEl.setAttribute('data-dpr', dpr);
+
+  // 设置 harilines 属性
+  // detect 0.5px supports
+  if (dpr >= 2) {
+    var fakeBody = document.createElement('body')
+    var testElement = document.createElement('div')
+    testElement.style.border = '.5px solid transparent'
+    fakeBody.appendChild(testElement)
+    docEl.appendChild(fakeBody)
+    if (testElement.offsetHeight === 1) {
+      docEl.classList.add('hairlines')
+    }
+    docEl.removeChild(fakeBody)
+  }
+
+  // set 1rem = viewWidth / 10
+  function refreshRem() {
+    var width = docEl.getBoundingClientRect().width;
+    /**
+     * 这个540其实是个经验值，或者最大值
+     * 目前主流手机最大的css像素尺寸，是540（比如devicePixelRatio为2，分辨率是1080x1920的手机），所以用了这个经验值。
+     * 这样可以让在ipad横屏这种情况下浏览无线页面，不至于因为拉伸适配后体验太差。
+     */
+    if (width > 540) {
+      width = 540;
+    }
+    var rem = width / 10;
+    docEl.style.fontSize = rem + 'px';
+  }
+
+  win.addEventListener('resize', function () {
+    clearTimeout(tid);
+    tid = setTimeout(refreshRem, 100);
+  }, false);
+
+  win.addEventListener('pageshow', function (e) {
+    if (e.persisted) {
+      clearTimeout(tid);
+      tid = setTimeout(refreshRem, 100);
+    }
+  }, false);
+
+  refreshRem();
+})(window, document);
